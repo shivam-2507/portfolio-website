778a1caa904784a6b179198a28e6fd843a69a942
Shivam Walia <shivamwalia2006@gmail.com>
2025-01-16 16:41:55 -0500

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,23 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Shivam Walia',
-  description: 'Shivam Walia Project Portfolio',
+    title: 'Shivam Walia',
+    description: 'Shivam Walia Project Portfolio',
+    icons: {
+        icon: '/Logo.png',      // Remove /public from path
+        shortcut: '/Logo.png',  // Remove /public from path
+        apple: '/Logo.png',     // Remove /public from path
+    },
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    )
 }
